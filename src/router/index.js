05c2a5e75e088c7eb88c94adf7cb14cedee2fb5b
Ádám Z. Kövér <adamzkover@gmail.com>
2025-03-05 17:22:23 +0100

    Configuration for GitHub pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PatientView from '@/views/PatientView.vue'
 import RegisterWound from '@/views/RegisterWound.vue'
@@ -6,7 +6,7 @@ import WoundView from '@/views/WoundView.vue'
 import RegisterObservation from '@/views/RegisterObservation.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
