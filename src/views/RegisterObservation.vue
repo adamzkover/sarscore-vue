d19d991de4ec9a7be65792e9f84f752bd098cd8e
Ádám Z. Kövér <adamzkover@gmail.com>
2025-03-06 13:12:14 +0100

    Add registration date to new observation form

--- src/views/RegisterObservation.vue
+++ src/views/RegisterObservation.vue
@@ -8,20 +8,34 @@
     </div>
     <h1>Register Observation</h1>
     <form @submit.prevent="submitForm">
-      <div>
-        <h2>Color</h2>
-        <label><input type="checkbox" v-model="color" value="pink" /> Pink</label>
-        <label><input type="checkbox" v-model="color" value="red" /> Red</label>
-        <label><input type="checkbox" v-model="color" value="black" /> Black</label>
-        <label><input type="checkbox" v-model="color" value="yellow" /> Yellow</label>
-        <label><input type="checkbox" v-model="color" value="gray" /> Gray</label>
-        <label><input type="checkbox" v-model="color" value="white" /> White</label>
+      <div class="row mb-3">
+        <div class="col-sm-2 col-form-label">
+          <p>Color</p>
+        </div>
+        <div class="col-sm-10">
+          <label><input type="checkbox" v-model="color" value="pink" /> Pink</label>
+          <label><input type="checkbox" v-model="color" value="red" /> Red</label>
+          <label><input type="checkbox" v-model="color" value="black" /> Black</label>
+          <label><input type="checkbox" v-model="color" value="yellow" /> Yellow</label>
+          <label><input type="checkbox" v-model="color" value="gray" /> Gray</label>
+          <label><input type="checkbox" v-model="color" value="white" /> White</label>
+        </div>
       </div>
-      <div>
-        <h2>Signs of Infection</h2>
-        <label><input type="checkbox" v-model="signsOfInfection" value="smell" /> Smell</label>
-        <label><input type="checkbox" v-model="signsOfInfection" value="pain" /> Pain</label>
-        <label><input type="checkbox" v-model="signsOfInfection" value="warmth" /> Warmth</label>
+      <div class="row mb-3">
+        <div class="col-sm-2 col-form-label">
+          <p>Infeksjonstegn</p>
+        </div>
+        <div class="col-sm-10">
+          <label><input type="checkbox" v-model="signsOfInfection" value="smell" /> Smell</label>
+          <label><input type="checkbox" v-model="signsOfInfection" value="pain" /> Pain</label>
+          <label><input type="checkbox" v-model="signsOfInfection" value="warmth" /> Warmth</label>
+        </div>
+      </div>
+      <div class="row mb-3">
+        <label for="registered" class="col-sm-2 col-form-label">Registered:</label>
+        <div class="col-sm-10">
+          <input id="registered" class="form-control" v-model="registered" type="date" :max="maxDate" required />
+        </div>
       </div>
       <button type="button" @click="cancel">Cancel</button>
       <button type="submit">Submit</button>
@@ -38,7 +52,9 @@ export default {
       patient: {},
       wound: {},
       color: [],
-      signsOfInfection: []
+      signsOfInfection: [],
+      registered: new Date().toISOString().split('T')[0],
+      maxDate: new Date().toISOString().split('T')[0]
     };
   },
   created() {
@@ -59,6 +75,7 @@ export default {
       // Handle form submission
       console.log('Color:', this.color);
       console.log('Signs of Infection:', this.signsOfInfection);
+      console.log('Registered:', this.registered);
     },
     cancel() {
       this.$router.push({ name: 'WoundView', params: { patientId: this.$route.params.patientId, woundId: this.$route.params.woundId } });
