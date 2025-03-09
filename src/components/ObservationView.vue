da491cbb950a3d2d8a8e4976d24bc1db2507e946
Ádám Z. Kövér <adamzkover@gmail.com>
2025-03-09 20:34:50 +0100

    Max-width image on sm screens and below

--- src/components/ObservationView.vue
+++ src/components/ObservationView.vue
@@ -3,7 +3,7 @@
     <div class="card-body">
       <div class="row">
         <div class="col-md-auto" v-if="observation.photo">
-          <img :src="observation.photo" alt="Observation Photo" style="max-width: 256px; max-height: 256px;" />
+          <img :src="observation.photo" alt="Observation Photo" class="observation-photo" />
         </div>
         <div class="col-md mt-3 mt-md-0">
           <p>Color: {{ observation.color.join(', ') }}</p>
@@ -26,5 +26,12 @@ const props = defineProps({
 </script>
 
 <style scoped>
-/* Add any styles for the ObservationView here */
+.observation-photo {
+  width: 100%;
+}
+@media (min-width: 768px) {
+  .observation-photo {
+    max-width: 256px;
+  }
+}
 </style>
