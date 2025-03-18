646ac6de60406531ebaea225fb94356b7f8e6c47
Ádám Z. Kövér <adamzkover@gmail.com>
2025-03-18 19:20:46 +0100

    Increase font size and use bold section titles on observation form

--- src/views/RegisterObservation.vue
+++ src/views/RegisterObservation.vue
@@ -8,13 +8,13 @@
       <br/>Registrert: {{ wound.registered }}
     </div>
     <h1 class="mt-3">Registrer sårobservasjon</h1>
-    <form @submit.prevent="submitForm" class="mt-3">
+    <form @submit.prevent="submitForm" class="fs-5 mt-3">
       <div class="row mb-3">
         <div class="col-sm-2">
           <h2><span class="times-icon fa-solid fa-ruler"></span></h2>
         </div>
         <div class="col-sm-10">
-          <p>Størrelse:</p>
+          <p class="fw-bold">Størrelse:</p>
           <div class="input-group mb-3">
             <span class="input-group-text" style="width: 5rem;" id="inputGroup-length">Lengde</span>
             <input type="number" inputmode="decimal" class="form-control" v-model="length" aria-label="Lengde" aria-describedby="inputGroup-length">
@@ -37,24 +37,24 @@
           <h2><span class="times-icon">T</span></h2>
         </div>
         <div class="col-sm-10">
-          <p>Farge:</p>
-          <div class="form-check">
+          <p class="fw-bold">Farge:</p>
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="color" type="checkbox" value="pink" id="colorPink" />
             <label class="form-check-label" for="colorPink"><span class="badge" style="background-color: #f2d3e1;">&nbsp;</span> Rosa</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="color" type="checkbox" value="red" id="colorRed" />
             <label class="form-check-label" for="colorRed"><span class="badge" style="background-color: #e30b5c;">&nbsp;</span> Rødt (friskt granulasjonsvev)</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="color" type="checkbox" value="wineRed" id="colorWineRed" />
             <label class="form-check-label" for="colorWineRed"><span class="badge" style="background-color: #8b0000;">&nbsp;</span> Vinrød (mulig lokal infeksjon)</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="color" type="checkbox" value="black" id="colorBlack" />
             <label class="form-check-label" for="colorBlack"><span class="badge text-bg-dark">&nbsp;</span> Svart (nekrose)</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="color" type="checkbox" value="yellow" id="colorYellow" />
             <label class="form-check-label" for="colorYellow"><span class="badge text-bg-warning">&nbsp;</span> Gult (nekrose)</label>
           </div>
@@ -65,56 +65,56 @@
           <h2><span class="times-icon">I</span></h2>
         </div>
         <div class="col-sm-10">
-          <p>Infeksjonstegn:</p>
-          <div class="form-check">
+          <p class="fw-bold">Infeksjonstegn:</p>
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="normalPhase" id="infNormalPhase" />
             <label class="form-check-label" for="infNormalPhase">Normal inflammasjonsfase</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="delayedHealing" id="infDelayedHealing" />
             <label class="form-check-label" for="infDelayedHealing">Forsinket sårtilheling</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="swelling" id="infSwelling" />
             <label class="form-check-label" for="infSwelling">Hevelse</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="easilyBleedingWoundBed" id="infEasilyBleedingWoundBed" />
             <label class="form-check-label" for="infEasilyBleedingWoundBed">Lettblødende sårbunn</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="localiedHeat" id="infLocalizedHeat" />
             <label class="form-check-label" for="infLocalizedHeat">Lokal varme</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="pocketFormation" id="infPocketFormation" />
             <label class="form-check-label" for="infPocketFormation">Lommedannelse</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="smell" id="infSmell" />
             <label class="form-check-label" for="infSmell">Lukt</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="discoloredExudate" id="infDiscoloredExudate" />
             <label class="form-check-label" for="infDiscoloredExudate">Misfarget sårvæske</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="rubor" id="infRubor" />
             <label class="form-check-label" for="infRubor">Rødme/rubor</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="slimySurface" id="infSlimySurface" />
             <label class="form-check-label" for="infSlimySurface">Slimete overflate</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="increasingPain" id="infIncreasingPain" />
             <label class="form-check-label" for="infIncreasingPain">Økende smerte</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="increasingSize" id="infIncreasingSize" />
             <label class="form-check-label" for="infIncreasingSize">Økende størrelse</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="signsOfInfection" type="checkbox" value="hypergranulation" id="infHypergranulation" />
             <label class="form-check-label" for="infHypergranulation">Hypergranulering</label>
           </div>
@@ -125,7 +125,7 @@
           <h2><span class="times-icon">M</span></h2>
         </div>
         <div class="col-sm-10">
-          <p>Er såret fuktig?</p>
+          <p class="fw-bold">Er såret fuktig?</p>
           <div class="form-check form-switch">
             <input class="form-check-input" v-model="isMoist" type="checkbox" role="switch" value="isMoist" id="infIsMoist">
             <label class="form-check-label" for="infIsMoist">Ja/Nei</label>
@@ -151,27 +151,27 @@
           </div>
           <div class="mt-3">
             <p>Farge på fuktighet:</p>
-            <div class="form-check">
+            <div class="form-check fs-5">
               <input class="form-check-input" v-model="moistureColor" type="checkbox" value="serøs" id="moistureSerous" :disabled="!isMoist" />
               <label class="form-check-label" for="moistureSerous">Serøs</label>
             </div>
-            <div class="form-check">
+            <div class="form-check fs-5">
               <input class="form-check-input" v-model="moistureColor" type="checkbox" value="hvit" id="moistureWhite" :disabled="!isMoist" />
               <label class="form-check-label" for="moistureWhite">Blakket</label>
             </div>
-            <div class="form-check">
+            <div class="form-check fs-5">
               <input class="form-check-input" v-model="moistureColor" type="checkbox" value="gul" id="moistureYellow" :disabled="!isMoist" />
               <label class="form-check-label" for="moistureYellow">Gul</label>
             </div>
-            <div class="form-check">
+            <div class="form-check fs-5">
               <input class="form-check-input" v-model="moistureColor" type="checkbox" value="grønn" id="moistureGreen" :disabled="!isMoist" />
               <label class="form-check-label" for="moistureGreen">Grønn</label>
             </div>
-            <div class="form-check">
+            <div class="form-check fs-5">
               <input class="form-check-input" v-model="moistureColor" type="checkbox" value="rosa" id="moisturePink" :disabled="!isMoist" />
               <label class="form-check-label" for="moisturePink">Rosa</label>
             </div>
-            <div class="form-check">
+            <div class="form-check fs-5">
               <input class="form-check-input" v-model="moistureColor" type="checkbox" value="lys rød" id="moistureLightRed" :disabled="!isMoist" />
               <label class="form-check-label" for="moistureLightRed">Lys rød</label>
             </div>
@@ -190,16 +190,16 @@
           <h2><span class="times-icon">E</span></h2>
         </div>
         <div class="col-sm-10">
-          <p>Sårkanter:</p>
-          <div class="form-check">
+          <p class="fw-bold">Sårkanter:</p>
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="edge" type="checkbox" value="dry" id="edgeDry" />
             <label class="form-check-label" for="edgeDry">Tørr, hard, opphøyet hud</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="edge" type="checkbox" value="edema" id="edgeEdema" />
             <label class="form-check-label" for="edgeEdema">Ødemer i sårkanter</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="edge" type="checkbox" value="macerated" id="edgeMacerated" />
             <label class="form-check-label" for="edgeMacerated">Oppbløtte sårkanter</label>
           </div>
@@ -210,28 +210,28 @@
           <h2><span class="times-icon">S</span></h2>
         </div>
         <div class="col-sm-10">
-          <p>Omkringliggende hud:</p>
-          <div class="form-check">
+          <p class="fw-bold">Omkringliggende hud:</p>
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="surroundingSkin" type="checkbox" value="drySkin" id="skinDry" />
             <label class="form-check-label" for="skinDry">Tørr hud</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="surroundingSkin" type="checkbox" value="maceratedSkin" id="skinMacerated" />
             <label class="form-check-label" for="skinMacerated">Oppbløtt hud</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="surroundingSkin" type="checkbox" value="redIrritatedSkin" id="skinRedIrritated" />
             <label class="form-check-label" for="skinRedIrritated">Rød og irritert hud</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="surroundingSkin" type="checkbox" value="edematousSkin" id="skinEdematous" />
             <label class="form-check-label" for="skinEdematous">Ødematøs</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="surroundingSkin" type="checkbox" value="eczema" id="skinEczema" />
             <label class="form-check-label" for="skinEczema">Eksem</label>
           </div>
-          <div class="form-check">
+          <div class="form-check fs-5">
             <input class="form-check-input" v-model="surroundingSkin" type="checkbox" value="discoloration" id="skinDiscoloration" />
             <label class="form-check-label" for="skinDiscoloration">Fargeforandringer</label>
           </div>
@@ -242,7 +242,7 @@
           <h2><span class="times-icon fa-solid fa-camera"></span></h2>
         </div>
         <div class="col-sm-10">
-          <label for="photo" class="col-sm-2 col-form-label">Last opp bilde:</label>
+          <label for="photo" class="col-sm-2 col-form-label fw-bold">Last opp bilde:</label>
           <input id="photo" class="form-control" type="file" accept="image/*" capture="camera" @change="onFileChange" />
         </div>
       </div>
@@ -251,7 +251,7 @@
           <h2><span class="times-icon fa-solid fa-pencil"></span></h2>
         </div>
         <div class="col-sm-10">
-          <p>Kommentar:</p>
+          <p class="fw-bold">Kommentar:</p>
           <textarea v-model="comment" class="form-control" rows="5" style="width: 100%;"></textarea>
         </div>
       </div>
@@ -260,7 +260,7 @@
           <h2><span class="times-icon fa-solid fa-calendar-days"></span></h2>
         </div>
         <div class="col-sm-10">
-          <label for="registered" class="col-sm-4 col-form-label">Dato for observasjoner:</label>
+          <label for="registered" class="col-sm-4 col-form-label fw-bold">Dato for observasjoner:</label>
           <input id="registered" class="form-control" v-model="registered" type="date" :max="maxDate" required />
         </div>
       </div>
